60e51a0df93c204262aee988dfb1f26c561218d5
milos pridavok <milos.pridavok@innovis.sk>
2022-03-07 15:10:27 +0100

    pripravena homepage, aj pre mobil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,88 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>trinitee is coming soon.</title>
 
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    </head>
-    <body>
-<h1>aaa</h1>
-    </body>
+    <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image" sizes="192x192"  href="img/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image" sizes="32x32" href="img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image" sizes="96x96" href="img/favicon/favicon-96x96.png">
+    <link rel="icon" type="image" sizes="16x16" href="img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="img/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="img/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/251a70c0d2.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar">
+    <div class="navbar__inner">
+        <div class="navbar__logo">
+            <a href="/"><img src="{{asset('img/trinitee-logo_dark.svg')}}" alt="trinitee"></a>
+        </div>
+
+        <a href="https://www.linkedin.com/company/trinitee" target="_blank" class="navbar__icon">
+            <i class="fa-brands fa-linkedin-in"></i>
+        </a>
+    </div>
+
+</nav>
+<main class="main wrapper">
+
+    <div class="main__left">
+        <h1>We’re coming soon</h1>
+        <a href="mailto:hello@trinitee.com" class="btn btn--white">Contact Us<i class="fa-solid fa-envelope"></i></a>
+    </div>
+
+    <div class="main__right">
+        <img class="img1" src="{{asset('img/trinitee-logo_cs.svg')}}" alt="trinitee progress">
+
+    <div class="progress">
+
+        <div class="progress__number">
+            78%
+        </div>
+
+        <div class="progress__text">
+            complete
+        </div>
+    </div>
+    </div>
+
+</main>
+
+
+<footer class="footer">
+
+    <div class="footer__column">
+        <p>trinitee, s.r.o.</p>
+    </div>
+    <div class="footer__column">
+        <a href="mailto:hello@trinitee.com">hello@trinitee.com</a>
+    </div>
+    <div class="footer__column">
+        <p>Komenského 11A<br>
+            040 01 Košice<br>
+            Slovakia</p>
+    </div>
+    <div class="footer__column">
+        <img src="{{asset('img/contact-map.svg')}}" alt="">
+    </div>
+
+</footer>
+</body>
 </html>
